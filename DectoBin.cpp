5d656390f0ea4a814ccc38d122d3ecd81a98e6b2
Cao Tam Kiên <kiencthe140322@fpt.edu.vn>
2019-05-21 22:02:11 +0700

    Update DectoBin.cpp
    Changed int to long long int #9billions

--- DectoBin.cpp
+++ DectoBin.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-	int a[100], n, i, k = -1;
+	long long int a[64], n, i, k = -1;
 	printf("\nDecimal to binary converter\n");
 	while (1)
 	{
 		printf("Enter a positive integer here:\t");
-		scanf("%d", &n);
+		scanf("%lld", &n);
 		printf("Your binary is ");
 		for (i = 0; n > 0; ++i)
 		{
@@ -15,7 +15,7 @@ int main(){
 			k++;
 		}
 		for (k; k >= 0; k--)
-			printf("%d", a[k]);
+			printf("%lld", a[k]);
 		printf("\n");
 		system("pause");
 	}
